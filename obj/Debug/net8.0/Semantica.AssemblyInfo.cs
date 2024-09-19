9424124ed22cb62effd2724aa31984d9ce8a3f58
AngelJMP <l22140763@queretaro.tecnm.mx>
2024-09-19 09:56:06 -0600

    19 Sep

--- obj/Debug/net8.0/Semantica.AssemblyInfo.cs
+++ obj/Debug/net8.0/Semantica.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Semantica")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90c3aabec197b191b3d9ac58f20754fca3b22ce0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e298b55a1df9dcc65e789a123b2c1949b3a81a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Semantica")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Semantica")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
